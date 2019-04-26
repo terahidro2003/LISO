74d4ac5ab87fa79db5975613c8ed514d8b5d0f6f
Juozas Skarbalius <juozas@seaportdevelopers.lt>
2019-04-26 18:10:42 +0300

    Update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,9 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
-        </div>
+    <div class="row-md-6 justify-content-center">
+        <h1>Sveiki atvykę į atsinaujinusią Jūsų studijos sistemą (v0.3.1)</h1>
+        <h3>Kūrimo darbai: Juozas Skarbalius ir Liudas Kasperavičius.</h3>
     </div>
 </div>
 @endsection
